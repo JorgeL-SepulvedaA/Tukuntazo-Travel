10c88fc40e7816fbe369980184ec46958511086b
Isael Diroche <isaeldiroche00@gmail.com>
2023-08-16 16:59:52 -0400

    feat: auth signup and login
    Inicio de Sesion y Registro completados

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:tukuntazo_travel/pages/authPage.dart';
 import 'package:flutter/material.dart';
-import 'package:tukuntazo_travel/auth/login_screen.dart';
-import 'package:tukuntazo_travel/firebase_options.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MainApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TukuntazoTravel',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home:
-          const LoginScreen(),
+      home: const AuthPage(),
     );
   }
 }
---
+++ lib/pages/loginOrRegister.dart
@@ -0,0 +1,33 @@
+import 'package:tukuntazo_travel/pages/loginPage.dart';
+import 'package:tukuntazo_travel/pages/registerpage.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({Key? key}) : super(key: key);
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  bool showLoginPage = true;
+
+  void togglePage() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePage,
+      );
+    } else {
+      return RegisterPage(
+        onTap: togglePage,
+      );
+    }
+  }
+}
