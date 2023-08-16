cbb0ba90dd6f936ace83a43f26f9993078c1d8af
Isael Diroche <isaeldiroche00@gmail.com>
2023-08-16 18:32:59 -0400

    update(googleMapScreen): añadidos locations points

--- lib/pages/googleMapScreen.dart
+++ lib/pages/googleMapScreen.dart
@@ -1,5 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:tukuntazo_travel/pages/locations.dart' as locations;
 
 class GoogleMapsWidget extends StatefulWidget {
   const GoogleMapsWidget({super.key});
@@ -10,17 +11,38 @@ class GoogleMapsWidget extends StatefulWidget {
 
 class _GoogleMapsWidgetState extends State<GoogleMapsWidget> {
   late GoogleMapController mapController;
+  final LatLng _center =
+      const LatLng(37.7749, -122.4194); // Ubicación inicial (San Francisco, CA)
 
+  final Map<String, Marker> _markers = {};
 
-  final LatLng _center = const LatLng(37.7749, -122.4194); // Ubicación inicial (San Francisco, CA)
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-      onMapCreated: (GoogleMapController controller) {
-        mapController = controller;
-      },
-      initialCameraPosition: CameraPosition(target: _center, zoom: 12),
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(0, 0),
+        zoom: 2,
+      ),
+      markers: _markers.values.toSet(),
     );
   }
 }
